7f8bc1ab7f3ea559f210e34a38e9554a1dd5080b
Unknown <Coryhemmelgarn@gmail.com>
2017-01-25 11:01:56 -0700

    Added ChangeColor with Unity changing 6 colors

---
+++ New Unity Project/Assets/ChangeColor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            GetComponent<Renderer>().material.color = Color.red;
+            print("Changed color to red.");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+            print("Changed color to Green.");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+            print("Changed color to Blue.");
+        }
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            print("Changed color to Blue.");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+            print("Changed color to Blue.");
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+            print("Changed color to Blue.");
+        }
+
+
+
+    }
+}
