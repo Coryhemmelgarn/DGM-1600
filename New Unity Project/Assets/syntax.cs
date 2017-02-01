620b9cb0fd0b83b1293c19e40d905314759bedaf
Unknown <Coryhemmelgarn@gmail.com>
2017-02-01 10:23:31 -0700

    added a git.ignore file

---
+++ New Unity Project/Assets/syntax.cs
@@ -0,0 +1,28 @@
+﻿//Class identifiers are sentence casing
+//for more than onw word titles UseCamelCasing
+public class SyntaxClass
+{
+    void ShootLaser()
+    {
+        print("Hello World");//each single line ends with a semi-colon
+        if (2 + 2 == 4)
+        {
+            print("It's True");
+        }
+    }
+
+    void ShootLaser()
+    {
+        if (10 + 10 == 30)
+        {
+            print("false");
+        }
+    }
+
+    //= is an assigning operator
+    //== is a comparison operator
+
+
+
+
+}
