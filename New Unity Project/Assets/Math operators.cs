4eb13b6531e90b030df2dbeedc64ec8608213bdd
Unknown <Coryhemmelgarn@gmail.com>
2017-02-03 10:41:36 -0700

    added math operators

---
+++ New Unity Project/Assets/Math operators.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mathoperators : MonoBehaviour {
+
+    public int addition = 12 + 4;
+    public int subtraction = 12 - 4;
+    public float division = 5.0f / 2.0f;
+    public int multiplication = 10 * 2;
+    public int remainder = 7 % 2;
+
+
+
+	
+	void Start () {
+        addition += 9;//+= means add to what we have up top.
+        subtraction -= 10;//subtract from 
+        addition += 3;
+        addition = 1; 
+	}
+	
+
+}
