4fe73649071248bfaaf94148e75bb42ddf344d8e
Unknown <Coryhemmelgarn@gmail.com>
2017-02-13 10:52:08 -0700

    Added "loops" and "iftoswitch"

---
+++ New Unity Project/Assets/IfToSwitch.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfToSwitch : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	    //Check for truth with one object
+        if(2+2 == 4)
+        {
+            print(true);
+        }
+        if(2+2 !=5)//the exclamation point is checking to see if something is false
+        {
+            print(true);
+        }
+
+        int nums = 2 + 2;
+        //check for true and false in a single statement
+        if(nums == 5)
+        {
+            print(true);
+        }
+        else
+        {
+            print(false);
+        }
+        //check for three or more values
+        switch(nums)
+        {
+            case 6:
+                print("Well sone you have" + nums);
+                break;
+            case 7:
+                print("Too bad, you have" + nums);
+                break;
+            case 8:
+                print("Stop drilling, you have" + nums);
+                break;
+
+            default:
+                print(nums);
+                break;
+        }
+        
+
+
+
+
+    }
+
+	
+
+}
