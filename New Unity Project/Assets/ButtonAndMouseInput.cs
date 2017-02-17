8f1e7db404b526e955aa1d344c4ea0d48d534591
Unknown <Coryhemmelgarn@gmail.com>
2017-02-17 10:45:32 -0700

    Button and mouse input and key input

---
+++ New Unity Project/Assets/ButtonAndMouseInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAndMouseInput : MonoBehaviour {
+
+	public void MyMouseClick()
+    {
+        print("I clicked the Button");
+    }
+    //Go to the button and click on button, look for the box and look for the "on click" box. Drag the button in the far left pane and drag it over. Click on "No Function" and then buttonandmouseinput and look for the name of your script. Click on it.
+    //Game object -> UI -> Button
+
+
+}
