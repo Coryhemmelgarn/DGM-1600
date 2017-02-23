f15be7a38cfb5a13677eb5c37a0bcc5dec2656d1
Coryhemmelgarn <Coryhemmelgarn@gmail.com>
2017-02-22 22:48:53 -0700

    Add files via upload
    Added lists for homework

---
+++ ListsHW.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lists : MonoBehaviour
+    //to be honest I was totally lost during class today while we were going over lists. I am going to watch the video you have posted to try and learn it but I would love to come in or stay after class one day to get a little extra help. I dont have our other files for reference because we went a little too fast.
+
+{
+
+    public Lists myInventory;
+    public string myWeapon;
+
+    public void AddToList()
+
+    {
+
+        myInventory.myWeaponList.Add(myWeapon);
+
+    }
+}
