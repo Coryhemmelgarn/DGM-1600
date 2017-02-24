5ab0b072350968e363cafb2173e1998266f47368
Unknown <Coryhemmelgarn@gmail.com>
2017-02-24 10:54:23 -0700

    added files from class

---
+++ New Unity Project/Assets/ForLoops.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ForLoops : MonoBehaviour {
+    public List<string> cars;
+
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < cars.Count; i++)
+        {
+            print(cars[i]);
+        }    
+
+
+	}
+	
+	
+}
---
+++ New Unity Project/Assets/TeacherSolvingProblem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;//this is first, add this
+public class TeacherSolvingProblem : MonoBehaviour {
+    //creating a list, and a loop, check if an element in the list is cat, print that element.
+    public List<string> animals;//then this
+    
+
+	// Use this for initialization
+	void Start () {
+        foreach (string item in animals)
+        {
+            if (item == "Cat")
+            {
+                print(item);
+            }
+        }
+
+	}
+	
+	
+}
