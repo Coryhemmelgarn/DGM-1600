3d7ab2256de04d96ba31b9b0612920113413cc5d
Unknown <Coryhemmelgarn@gmail.com>
2017-02-27 10:49:26 -0700

    added button to list instructions

---
+++ New Unity Project/Assets/buttontolistinstructions.cs
@@ -0,0 +1,13 @@
+﻿//sources for help is at unity.com
+//buttonclick.cs
+public Inventory AnimalInventory;
+public string animal;
+public void AddtoList()
+{
+    AnimalInventory.animals.Add(animal)
+    }
+
+
+//in inventory.cs. Goes onto the game object
+public Lists<string> animals;
+
