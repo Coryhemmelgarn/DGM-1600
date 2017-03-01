36ca7fff38787dffcbc532bf8ddeb8b64b92adf9
Unknown <Coryhemmelgarn@gmail.com>
2017-03-01 10:48:04 -0700

    Foreach loops/switches/adding things to a list w/ button

---
+++ New Unity Project/Assets/ForEachLoops.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ForEachLoops : MonoBehaviour {
+    //in the far left pane, drag lists over to the onmouseclick of the button. On the function click check data().
+    public List<GameObject> powerUpList;
+    public List<GameObject> cubeList;
+    public List<GameObject> sphereList;
+    public List<GameObject> capsuleList;
+
+	// Use this for initialization
+	public void CheckData () {
+        foreach (GameObject item in powerUpList)
+        {
+            switch (item.name)
+            {
+                case "Cube":
+                    cubeList.Add(item);
+
+                    break;
+
+                case "Capsule":
+                    capsuleList.Add(item);
+
+                    break;
+
+                case "Sphere":
+                    sphereList.Add(item);
+
+                    break;
+            }
+        }
+	}
+	
+
+}
