a251575d2c7d4ec2f49e61c114b8b95a05718463
Unknown <Coryhemmelgarn@gmail.com>
2017-03-06 10:52:42 -0700

    access modifiers

---
+++ New Unity Project/Assets/scope.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scope : MonoBehaviour {
+
+    public string owner = "Bob";
+    private string renter = "Kate";
+
+   public void Awake()
+    {
+        string cat = "Joe";
+        if (cat == "Joe")
+        {
+            print(renter);
+            string mouse = "Helen";
+            print(cat+ " ate "+ mouse);
+        }
+        print(cat);
+        print(owner);
+    }
+
+	void Start () {
+        string cat = "Frank";
+        print(cat);
+        print(owner);
+	}
+	
+	
+}
