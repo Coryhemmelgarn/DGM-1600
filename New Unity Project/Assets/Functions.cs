07352153465d6a3adfce9b7a705b66e9c559297a
Unknown <Coryhemmelgarn@gmail.com>
2017-03-10 10:44:17 -0700

    added functions

---
+++ New Unity Project/Assets/Functions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Functions : MonoBehaviour {
+    //Taking a big group of code and grouping it together into one function. It will all be executed at run time. A void function runs a set of commends. Doesnt give anything back, it just runs a set of commands. Doesnt require input, and gives no output.
+    int num1 = 5;
+    int num2 = 3;
+
+	public void AddNumbers()
+    {
+        int sum;
+        sum = num1 + num2;
+        print(sum);
+    }
+}
