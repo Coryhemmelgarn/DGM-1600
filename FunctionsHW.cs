eeb4363470a2488a4eeecf065ff563addda9dff7
Coryhemmelgarn <Coryhemmelgarn@gmail.com>
2017-03-11 01:44:41 -0700

    Add files via upload
    Added Void functions for homework

---
+++ FunctionsHW.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class Functions : MonoBehaviour
+{
+
+    //Taking a big group of code and grouping it together into one function. It will all be executed at run time. A void function runs a set of commends. Doesnt give anything back, it just runs a set of commands. Doesnt require input, and gives no output.
+
+    int num1 = 1;
+    int num2 = 2;
+    int num3 = 3;
+    int num4 = 4;
+    int num5 = 5;
+    int num6 = 6;
+    int num7 = 7;
+    int num8 = 8;
+    int num9 = 9;
+    int num10 = 10;
+
+    public void AddNumbers()
+        
+    {
+        int sum;
+        sum = num1 + num2;
+        print(sum);
+    }
+   
+
+    
+   
+
+}
+
