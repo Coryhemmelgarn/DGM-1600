3c28325dac4e4318298a89ae93a7430948337ab6
Unknown <Coryhemmelgarn@gmail.com>
2017-03-15 10:51:18 -0600

    Return Functions 1/2

---
+++ New Unity Project/Assets/ReturnFunctions2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnFunctions2 : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject firstPlayer;
+
+    GameObject ChooseFirstPlayer ()
+    {
+        int randNum = Random.Range(1, 3);
+        switch (randNum)
+        {
+            case 1:
+                return player1;
+               
+            case 2:
+                return player2;
+
+            default:
+                return player3;
+               
+        }
+       
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        firstPlayer = ChooseFirstPlayer();
+	}
+	
+	
+}
