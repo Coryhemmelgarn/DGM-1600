d4956427e293781d8da8eb3c2bb59b53bb42f41a
Coryhemmelgarn <Coryhemmelgarn@gmail.com>
2017-03-29 10:46:13 -0600

    Add files via upload
    uploaded vars and functions

---
+++ VarsAndFunctions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarsAndFunctions : MonoBehaviour
+{
+
+    void RotateWheel()
+    {
+        //void means it functions (does work).
+        //it will rotate the wheel.
+    }
+
+    string WaterFountain()
+    {
+        return "Water";
+    }
+
+    float VendingMachine(float money)
+    {
+        float coke = 1.50f;
+        float change = 0;
+        if (money >= coke)
+        {
+            change = money - coke;
+            return 1.50f;
+        }
+        print("One coke dispensed");
+        return change;
+
+
+
+
+    }
+}
