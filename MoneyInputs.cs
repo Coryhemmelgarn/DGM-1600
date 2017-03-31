8f53bb894fa2d20ca3d63d1537c6beaa3abf7ed7
Coryhemmelgarn <Coryhemmelgarn@gmail.com>
2017-03-31 10:58:29 -0600

    Add files via upload
    Vending machine stuff

---
+++ MoneyInputs.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoneyInputs : MonoBehaviour {
+
+    public InputField money;
+    public float MoneyToFloat()
+    {
+        return 10;
+    }
+
+    
+}
---
+++ VendingButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VendingButton : MonoBehaviour {
+
+    public MoneyInputs moneyInput;
+
+    public string productName = "Coke";
+    public float productPrice = 1.5f;
+
+	float VendProduct(float money)
+    {
+        print("Vending " + productName + ".");
+        return money - productPrice;
+    }
+
+    public void ButtonClick()
+    {
+        print("$" + VendProduct(moneyInput.MoneyToFloat()) + " is your change");
+    }
+}
